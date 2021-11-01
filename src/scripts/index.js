4600d589fed649faa9e46db5049e07c3049e8254
eimwe <prokopenkoserge@inbox.ru>
2021-11-01 21:33:40 +0600

    Добавил функцию для переключения кнопок фильтра

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,1 +1,21 @@
 import '../styles/index.scss';
+
+function changeClub() {
+  let filterButtons = document.querySelectorAll('.teams__option');
+
+  filterButtons?.forEach(button => {
+
+    button.addEventListener('click', event => {
+      let clickedButton = event.target;
+      let activeButton = document.querySelector('.teams__option--active');
+
+      if (clickedButton !== activeButton) {
+        clickedButton.classList.add('teams__option--active');
+        activeButton?.classList.remove('teams__option--active');
+      }
+    });
+
+  });
+}
+
+changeClub();
