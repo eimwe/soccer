2b1206397ba24e9e2bd20f3bd88f2933b03cd0a5
eimwe <prokopenkoserge@inbox.ru>
2021-11-02 19:48:07 +0600

    Добавил мок-данные для клуба Тверь

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,13 @@
 import '../styles/index.scss';
 import { DINAMO_STARTERS, DINAMO_BACKUP, DINAMO_MENTORS } from './dinamo';
+import { TVER_STARTERS, TVER_BACKUP, TVER_MENTORS } from './tver';
 
 const DINAMO_REGULARS = DINAMO_STARTERS;
 const DINAMO_RESERVE = DINAMO_BACKUP;
 const DINAMO_COACHES = DINAMO_MENTORS;
+const TVER_REGULARS = TVER_STARTERS;
+const TVER_RESERVE = TVER_BACKUP;
+const TVER_COACHES = TVER_MENTORS;
 
 function changeClub() {
   let filterButtons = document.querySelectorAll('.teams__option');
---
+++ src/scripts/tver.js
@@ -0,0 +1,213 @@
+export const TVER_STARTERS = [
+  {
+    id: '1',
+    country: 'Россия',
+    name: 'Основной игрок 1',
+    isCaptain: true,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Врт',
+    roleDesc: 'Вратарь',
+  },
+  {
+    id: '2',
+    country: 'Россия',
+    name: 'Основной игрок 2',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '3',
+    country: 'Армения',
+    name: 'Основной игрок 3',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '4',
+    country: 'Россия',
+    name: 'Основной игрок 4',
+    isCaptain: false,
+    isMidField: true,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '5',
+    country: 'Россия',
+    name: 'Основной игрок 5',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+];
+
+export const TVER_BACKUP = [
+  {
+    id: '6',
+    country: 'Россия',
+    name: 'Запасной игрок 6',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [],
+    role: 'Врт',
+    roleDesc: 'Вратарь',
+  },
+  {
+    id: '7',
+    country: 'Россия',
+    name: 'Запасной игрок 7',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Нап',
+    roleDesc: 'Нападающий',
+  },
+  {
+    id: '8',
+    country: 'Россия',
+    name: 'Запасной игрок 8',
+    isCaptain: false,
+    isMidField: true,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '9',
+    country: 'Россия',
+    name: 'Запасной игрок 9',
+    isCaptain: false,
+    isMidField: true,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '10',
+    country: 'Россия',
+    name: 'Запасной игрок 10',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+    ],
+    role: 'Нап',
+    roleDesc: 'Нападающий',
+  },
+];
+
+export const TVER_MENTORS = [
+  {
+    id: 'Гт',
+    country: 'Россия',
+    name: 'Главный тренер',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: true,
+    moments: [],
+    role: 'Главный тренер',
+    roleDesc: '',
+  },
+  {
+    id: 'Пт',
+    country: 'Швеция',
+    name: 'Помощник тренера',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: true,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+    ],
+    role: 'Помощник тренера',
+    roleDesc: '',
+  },
+];
