bc3823d9498662703fb881b56e9e0bcd8fa87c44
eimwe <prokopenkoserge@inbox.ru>
2021-11-02 19:46:28 +0600

    Добавил мок-данные для клуба Динамо

---
+++ src/scripts/dinamo.js
@@ -0,0 +1,515 @@
+export const DINAMO_STARTERS = [
+  {
+    id: '1',
+    country: 'Россия',
+    name: 'Станислав Черчесов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Врт',
+    roleDesc: 'Вратарь',
+  },
+  {
+    id: '2',
+    country: 'Россия',
+    name: 'Юрий Гагарин',
+    isCaptain: true,
+    isMidField: false,
+    isCoach: false,
+    moments: [],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '3',
+    country: 'Россия',
+    name: 'Константин Константинов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '4',
+    country: 'Россия',
+    name: 'Сергей Игнашевич',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '5',
+    country: 'Россия',
+    name: 'Александр Кержаков',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '6',
+    country: 'Россия',
+    name: 'Сергей Семак',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '7',
+    country: 'Россия',
+    name: 'Константин Зырянов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '8',
+    country: 'Россия',
+    name: 'Андрей Аршавин',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '9',
+    country: 'Россия',
+    name: 'Валерий Карпин',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '10',
+    country: 'Россия',
+    name: 'Дмитрий Лоськов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+    ],
+    role: 'Нап',
+    roleDesc: 'Нападающий',
+  },
+  {
+    id: '11',
+    country: 'Россия',
+    name: 'Дмитирй Сычёв',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Нап',
+    roleDesc: 'Нападающий',
+  },
+];
+
+export const DINAMO_BACKUP = [
+  {
+    id: '12',
+    country: 'Россия',
+    name: 'Станислав Черчесов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Врт',
+    roleDesc: 'Вратарь',
+  },
+  {
+    id: '13',
+    country: 'Россия',
+    name: 'Игорь Акинфеев',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Врт',
+    roleDesc: 'Вратарь',
+  },
+  {
+    id: '14',
+    country: 'Россия',
+    name: 'Денис Колодин',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '15',
+    country: 'Россия',
+    name: 'Роман Широков',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '16',
+    country: 'Россия',
+    name: 'Дмитрий Хохлов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Зщ',
+    roleDesc: 'Защитник',
+  },
+  {
+    id: '17',
+    country: 'Россия',
+    name: 'Андрей Тихонов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '18',
+    country: 'Россия',
+    name: 'Федор Черенков',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '19',
+    country: 'Россия',
+    name: 'Эдуард Стрельцов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '20',
+    country: 'Россия',
+    name: 'Егор Титов',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Пз',
+    roleDesc: 'Полузащитник',
+  },
+  {
+    id: '21',
+    country: 'Россия',
+    name: 'Дмитрий Торбинский',
+    isCaptain: false,
+    isMidField: true,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Нап',
+    roleDesc: 'Нападающий',
+  },
+  {
+    id: '22',
+    country: 'Россия',
+    name: 'Магомед Шапи-Сулейманов',
+    isCaptain: false,
+    isMidField: true,
+    isCoach: false,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+      {
+        timestamp: '83',
+        result: 'Замена',
+      },
+    ],
+    role: 'Нап',
+    roleDesc: 'Нападающий',
+  },
+];
+
+export const DINAMO_MENTORS = [
+  {
+    id: 'Гт',
+    country: 'Россия',
+    name: 'Константин Бесков',
+    isCaptain: false,
+    isMidField: false,
+    isCoach: true,
+    moments: [
+      {
+        timestamp: '83',
+        result: 'Предупреждение',
+      },
+      {
+        timestamp: '83',
+        result: 'Удаление с поля',
+      },
+    ],
+    role: 'Главный тренер',
+    roleDesc: '',
+  },
+];
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,9 @@
 import '../styles/index.scss';
+import { DINAMO_STARTERS, DINAMO_BACKUP, DINAMO_MENTORS } from './dinamo';
+
+const DINAMO_REGULARS = DINAMO_STARTERS;
+const DINAMO_RESERVE = DINAMO_BACKUP;
+const DINAMO_COACHES = DINAMO_MENTORS;
 
 function changeClub() {
   let filterButtons = document.querySelectorAll('.teams__option');
