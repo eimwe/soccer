e5879905fc6530abf05067ffb7f26ca184c4d28c
eimwe <prokopenkoserge@inbox.ru>
2021-11-02 22:55:54 +0600

    Добавил функцию для создания событий матча

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -39,6 +39,71 @@ const getPlayerRole = (container, role, roleDesc, isCoach) => {
 
 /**
  * @function
+ * @name getPlayerMoments
+ * @description Создает ноды с событиями матча для каждого игрока.
+ * Если событий не было, создает специальную ноду messageNode
+ * @see {@link customizePlayerTemplate} для вызова описываемой функции
+ * @param {HTMLElement} container - playerMomentsContainer -->
+ * контейнер, куда прикрепляются созданные этой функцией ноды
+ * @param {Object[]} moments - Массив объектов, где зафиксированы время и события матча
+ * @returns {undefined}
+ */
+const getPlayerMoments = (container, moments) => {
+
+  if (moments.length < 1) {
+    let messageNode = document.createElement('SPAN');
+    let textNode = document.createTextNode('Не отличился');
+    messageNode.classList.add('visually-hidden');
+    messageNode.appendChild(textNode);
+    container.appendChild(messageNode);
+    return;
+  }
+  
+  moments.forEach(moment => {
+
+    let divNode = document.createElement('DIV');
+    divNode.classList.add('teams__timestamp', 'flex-container');
+  
+    for (const [key, value] of Object.entries(moment)) {
+        
+      if (key === 'timestamp') {
+        let timeNode = document.createElement('TIME');
+        let textNode = document.createTextNode(`${value}'`);
+        timeNode.setAttribute('datetime', value);
+        timeNode.appendChild(textNode);
+        divNode.appendChild(timeNode);
+      }
+
+      if (key === 'result') {
+        let spanNode = document.createElement('SPAN');
+        spanNode.classList.add('teams__outcome');
+
+        switch (value) {
+          case 'Предупреждение':
+            spanNode.classList.add('teams__outcome--yellow-card');
+              break;
+          case 'Удаление с поля':
+            spanNode.classList.add('teams__outcome--red-card');
+              break;
+          case 'Замена':
+            spanNode.classList.add('teams__outcome--replacement');
+              break;
+        }
+  
+        let textNode = document.createTextNode(value);
+        spanNode.appendChild(textNode);
+        divNode.appendChild(spanNode);
+      }
+
+    }
+
+    container.appendChild(divNode);
+  });
+
+}
+
+/**
+ * @function
  * @name customizePlayerTemplate
  * @description Клонирует темплейт,
  * добавляет нодам классы и принимает
@@ -99,6 +164,11 @@ const customizePlayerTemplate = (id,
    * Данная функция описана в @see {@link getPlayerRole}
    */
   getPlayerRole(playerRole, role, roleDesc, isCoach);
+
+  /**
+   * Данная функция описана в @see {@link getPlayerMoments}
+   */
+  getPlayerMoments(playerMomentsContainer, moments);
 
   return player;
 };
