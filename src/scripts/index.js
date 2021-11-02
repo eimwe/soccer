c997ce886bc8ff9648c322e107ef943dabc0bcea
eimwe <prokopenkoserge@inbox.ru>
2021-11-02 22:05:55 +0600

    Добавил функцию для клонирования темплейта

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,6 +9,67 @@ const TVER_REGULARS = TVER_STARTERS;
 const TVER_RESERVE = TVER_BACKUP;
 const TVER_COACHES = TVER_MENTORS;
 
+/**
+ * @function
+ * @name customizePlayerTemplate
+ * @description Клонирует темплейт,
+ * добавляет нодам классы и принимает
+ * деструктурированный массив объектов
+ * @param {string} id - порядковый номер в списке игроков
+ * @param {string} name - имя игрока
+ * @param {string} country - страна игрока
+ * @param {Boolean} isCaptain - капитан команды (true|false)
+ * @param {Boolean} isMidField - midfielder (true|false)
+ * @param {Boolean} isCoach - тренер (true|false)
+ * @param {string} role - аббревиатура роли игрока в команде
+ * @param {string} roleDesc - расшифровка аббревиатуры роли игрока в команде
+ * @param {Object[]} moments - Массив объектов, где зафиксированы время и события матча
+ * @returns {HTMLElement} player - Клон, готовый для рендеринга
+ */
+const customizePlayerTemplate = (id, 
+                                name, 
+                                country, 
+                                isCaptain, 
+                                isMidField, 
+                                isCoach, 
+                                role, 
+                                roleDesc, 
+                                moments) => {
+
+  let playerTemplate = document.getElementById('player'),
+      player = playerTemplate?.content.firstElementChild.cloneNode(true),
+      playerOrigin = player?.querySelector('.teams__country'),
+      playerName = player?.querySelector('.teams__name'),
+      playerRole = player?.querySelector('.teams__role'),
+      playerMomentsContainer = player?.querySelector('.teams__moments');
+
+  player.querySelector('.teams__number').textContent = id;
+  playerOrigin.textContent = country;
+
+  switch (country) {
+    case 'Армения':
+      playerOrigin?.classList.add('teams__country--armenia');
+      break;
+    case 'Швеция':
+      playerOrigin?.classList.add('teams__country--sweden');
+      break;
+    default:
+      playerOrigin?.classList.add('teams__country--russia');
+  }
+
+  playerName.textContent = name;
+
+  if (isCaptain) {
+    playerName?.classList.add('teams__name--captain');
+  }
+
+  if (isMidField) {
+    playerName?.classList.add('teams__name--midfielder');
+  }
+
+  return player;
+};
+
 function changeClub() {
   let filterButtons = document.querySelectorAll('.teams__option');
 
